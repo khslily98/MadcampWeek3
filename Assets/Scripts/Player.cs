71362b2db0264aa37a37fbc37fe0701cbac0f68d
Seongjae Im <seongjae114@gmail.com>
2020-07-25 20:16:08 +0900

    duck fight

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,29 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+[RequireComponent (typeof (ShotController))]
+public class Player : LivingEntity
 {
     private CharacterController controller;
+    private ShotController shotController;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    public float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
+    public int playerIndex;
+    private string[] horizontalAxis = {"Horizontal1", "Horizontal2"};
+    private string[] verticalAxis = {"Vertical1", "Vertical2"};
+    private string[] jumpKey = {"Jump1", "Jump2"};
+    private KeyCode[] shotKey = {KeyCode.Slash, KeyCode.Z};
 
-    private void Start()
+    protected override void Start()  
     {
+        base.Start();
         controller = gameObject.GetComponent<CharacterController>();
+        shotController = gameObject.GetComponent<ShotController>();
     }
 
     void Update()
     {
+        // Movement Control
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = new Vector3(Input.GetAxis(horizontalAxis[playerIndex-1]), 0, Input.GetAxis(verticalAxis[playerIndex-1]));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
@@ -33,12 +43,18 @@ public class Player : MonoBehaviour
         }
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (Input.GetButtonDown(jumpKey[playerIndex-1]) && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        //Weapon Control
+        if (Input.GetKey(shotKey[playerIndex-1])) {
+			shotController.Shoot();
+		}
     }
+
 }
---
+++ Assets/Scripts/ShotController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    public Transform muzzle;
+    public Projectile startingProjectile;
+    public float msBetweenShots = 100;
+    public float muzzleVelocity = 35;
+    Projectile equippedProjectile;
+    float nextShotTime = 0;
+
+    void Start() {
+        if(startingProjectile != null ) {
+            EquipProjectile(startingProjectile);
+        }
+    }
+    public void EquipProjectile(Projectile projectileToEquip) {
+        equippedProjectile = projectileToEquip;
+    }
+
+    public void Shoot() {
+        if(equippedProjectile != null && Time.time > nextShotTime){
+            nextShotTime = Time.time + msBetweenShots / 1000;
+            Projectile newProjectile = Instantiate(equippedProjectile, muzzle.position, muzzle.rotation);
+            newProjectile.SetSpeed(muzzleVelocity);
+        }
+        
+    }
+}
