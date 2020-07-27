eafb862ce0fe5f0c5cea8e9b3b9c180d05fa294f
Seongjae Im <seongjae114@gmail.com>
2020-07-27 16:19:36 +0900

    Added menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance {
+        get {
+            if(m_instance == null){
+                m_instance = FindObjectOfType<GameManager>();
+            }
+            return m_instance;
+        }
+    }
+    
+    public bool isGameover { get; private set; }
+
+    private static GameManager m_instance;
+    void Awake() {
+        if(instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        
+    }
+    // void Update()
+    // {
+        
+    // }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuHolder;
+    public GameObject optionsMenuHolder;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Play1P()
+    {
+        
+    }
+
+    public void Play2P()
+    {
+        SceneManager.LoadScene("PondGame");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+}
